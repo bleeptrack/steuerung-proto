09c0e6af349701ac2ce12c1150627bfab27fa17e
Unknown <sabine.wieluch@gmail.com>
2018-11-14 15:59:33 +0100

    schliessen intent

--- action-schliessen.py
+++ action-schliessen.py
@@ -30,7 +30,7 @@ def subscribe_intent_callback(hermes, intentMessage):
 
 def action_wrapper(hermes, intentMessage, conf):
     current_session_id = intentMessage.session_id
-    hermes.publish_end_session(current_session_id, 'Jup, mache auf')
+    hermes.publish_end_session(current_session_id, 'Wird geschlossen.')
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
